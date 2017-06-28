8428b81125317bab4f5de935ec05b8dabce9639d
sasikalacse <sasikalas936@gmail.com>
2017-06-28 12:53:38 +0530

    repeated numbers order
    c programme

---
+++ order.C
@@ -0,0 +1,8 @@
+traverse the list for i=0 to n-1 elements
+{
+check for sign of A[abs(A[i])];
+if positive then
+make it negative by A[abs(A[i])]=-A[abs(A[i])];
+else //i.e..,A[abs(A[i])] is negative
+this element (ith element of the list) is repeatation
+}
